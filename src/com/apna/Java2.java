e77804c64c1c877f9c7e54c2058a5ec7968f99bf
Pankaj <ps533502@gmail.com>
2021-10-04 17:16:36 +0530

    Initial commit

---
+++ src/com/apna/Java2.java
@@ -0,0 +1,22 @@
+package com.apna;
+
+public class Java2 {
+//    private static final Object IS = 15;
+//    private static final Object IS = ;
+
+    public static void main(String[] args) {
+
+
+        //      System.out.println("This is Time");
+
+//        byte age = 34;
+//        char ch = 'A';
+//        float f1 = 5.6f;
+//        double d1 = 4.66D;
+//        System.out.println(d1);
+
+
+
+    }
+}
+
---
+++ src/com/apna/Main.java
@@ -0,0 +1,84 @@
+package com.apna;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+//        int age = 24;
+//        int AGE = 25;
+//
+//        System.out.println(age);  // prints 24
+//        System.out.println(AGE);  // prints 25
+
+//        double myDouble = 3.4;
+//        float myFloat = 3.4F;
+
+        // 3.445*10^2
+//        double myDoubleScientific = 3.445e2;
+//
+//        System.out.println(myDouble);  // prints 3.4
+//        System.out.println(myFloat);    // prints 3.4
+//        System.out.println(myDoubleScientific);   // prints 344.5
+
+//
+        // declare variables
+//        int a = 12, b = 5;
+//
+//        // addition operator
+//        System.out.println("a + b = " + (a + b));
+//
+//        // subtraction operator
+//        System.out.println("a - b = " + (a - b));
+//
+//        // multiplication operator
+//        System.out.println("a * b = " + (a * b));
+//
+//        // division operator
+//        System.out.println("a / b = " + (a / b));
+//
+//        // modulo operator
+//        System.out.println("a % b = " + (a % b));
+
+
+        // create variables
+//        int a = 7, b = 11;
+//
+//        // value of a and b
+//        System.out.println("a is " + a + " and b is " + b);
+//
+//        // == operator
+//        System.out.println(a == b);  // false
+//
+//        // != operator
+//        System.out.println(a != b);  // true
+//
+//        // > operator
+//        System.out.println(a > b);  // false
+//
+//        // < operator
+//        System.out.println(a < b);  // true
+//
+//        // >= operator
+//        System.out.println(a >= b);  // false
+//
+//        // <= operator
+//        System.out.println(a <= b);  // true
+
+
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int sum = num1 + num2 + num3;
+        System.out.println("sumn");
+
+
+
+
+
+
+
+        }
+    }
+
+
